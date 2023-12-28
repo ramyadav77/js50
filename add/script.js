3bf7a95dfffd44afa6651a5cb739da0ee9cbecb6
Ram Pravesh <rampraveshyadav30122000@gmail.com>
2023-12-28 15:34:14 +0530

    addition program

---
+++ add/script.js
@@ -0,0 +1,18 @@
+/* 
+const num1=5;
+const num2=10;
+// add two numbers
+const sum=num1+num2;
+// display the sum
+console.log(`the sum of ${num1} and ${num2} is ${sum}`);
+*/
+
+// store input numbers
+const num1=parseInt(prompt('enter the first number'));
+const num2=parseInt(prompt('enter the second number'));
+
+// add two numbers
+const sum=num1+num2;
+
+// display the sum
+console.log(`the sum of ${num1} and ${num2} is ${sum}`);
